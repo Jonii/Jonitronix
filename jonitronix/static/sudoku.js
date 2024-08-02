eedd48fdda1ac4a265632938f93d89ac09d97f9a
Jonii <joni.hanski@gmail.com>
2024-08-02 15:31:19 +0200

    Added select mode button

--- jonitronix/static/sudoku.js
+++ jonitronix/static/sudoku.js
@@ -15,45 +15,68 @@ sudokuboard.setBoard(`
   _ _ _ | _ _ _ | _ _ _
   `)
 
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'Control') {
-        sudokuboard.selectMode = "multi";
-    }
-});
 
-document.addEventListener('keyup', function (e) {
-    if (e.key === 'Control') {
-      sudokuboard.selectMode = "single";
-    }
-});
 
 document.addEventListener('keydown', function (e) {
     const inputNum = e.key;
     if (inputNum >= '1' && inputNum <= '9') {
-        sudokuboard.enterNumberToSelected(parseInt(inputNum));
+      sudokuboard.enterNumberToSelected(parseInt(inputNum));
         return;
     }
     if (inputNum == 'Backspace') {
         sudokuboard.deleteContentFromSelected();
-    }
-});
-
+      }
+    });
+    
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("board").replaceWith(sudokuboard.htmlParent);
+  document.getElementById("board").replaceWith(sudokuboard.htmlParent);
 
-    // Handle number button clicks
-
-    const controlPanel = document.getElementById("controls");
-    const buttons = controlPanel.querySelectorAll(".number-container button");
-    for (const button of buttons) {
-      button.addEventListener("click", () => {
-        sudokuboard.enterNumberToSelected(button.getAttribute("data-number"));
-      })
-    }
-    const newGameButton = document.getElementById("new-game");
-    newGameButton.addEventListener("click", () => {
-      sudokuboard.reset();
+  // Handle number button clicks
+  
+  const controlPanel = document.getElementById("controls");
+  const buttons = controlPanel.querySelectorAll(".number-container button");
+  for (const button of buttons) {
+    button.addEventListener("click", () => {
+      sudokuboard.enterNumberToSelected(button.getAttribute("data-number"));
     })
+  }
+  const newGameButton = document.getElementById("new-game");
+  newGameButton.addEventListener("click", () => {
+    sudokuboard.reset();
+  })
+  const selectModeBtn = document.getElementById("selectMode");
+  console.log(selectModeBtn);
+  const toggleSelectMode = () => {
+  const oldMode = selectModeBtn.value;
+    if (oldMode === "multi" || oldMode === "") {
+      selectModeBtn.textContent = "Inputmode: single";
+      selectModeBtn.value = "single";
+      sudokuboard.selectMode = "single";
+      console.log("Setting mode to single");
+      return;
+    }
+    console.log("Setting mode to multi");
+    selectModeBtn.textContent = "Inputmode: multi";
+    selectModeBtn.value = "multi";
+    sudokuboard.selectMode = "multi";
+  
+  }
+  document.addEventListener('keydown', function (e) {
+      if (e.key === 'Control') {
+        toggleSelectMode();
+      }
+  });
 
+  document.addEventListener('keyup', function (e) {
+      if (e.key === 'Control') {
+        toggleSelectMode();
+      }
+  });
+
+
+  selectModeBtn.addEventListener("click", (ev) => {
+    toggleSelectMode();
+  })
+  toggleSelectMode();
 
 });
